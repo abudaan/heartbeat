390bd71d06f5861acbdb0b9ec3be0d0b4632f7be
abudaan <abudaan@gmail.com>
2018-08-06 16:08:09 +0200

    build tool

--- tools/build.sh
+++ tools/build.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 files="../src/license.js
-    ../src/jzz/JZZ.js
+    ../src/chris-wilson/WebMIDIAPIShim.js
     ../src/w3c-polyfill/saveAs.js
     ../src/heartbeat/open_module.js
     ../src/heartbeat/asset_manager.js
     ../src/heartbeat/assetpack.js
-    ../src/heartbeat/audio_encoder.js
     ../src/heartbeat/audio_event.js
     ../src/heartbeat/audio_recorder.js
-    ../src/heartbeat/audio_recorder_worker.js
     ../src/heartbeat/audio_track.js
     ../src/heartbeat/channel_effects.js
     ../src/heartbeat/event_statistics.js
     ../src/heartbeat/find_event.js
     ../src/heartbeat/instrument.js
-    ../src/heartbeat/instrument2.js
     ../src/heartbeat/instrument_config.js
-    ../src/heartbeat/instrument_methods.js
     ../src/heartbeat/key_editor.js
     ../src/heartbeat/key_editor_iterator_factory.js
     ../src/heartbeat/metronome.js
@@ -28,13 +24,13 @@ files="../src/license.js
     ../src/heartbeat/midi_stream.js
     ../src/heartbeat/midi_system.js
     ../src/heartbeat/midi_write.js
-    ../src/heartbeat/musicxml_parser.js
     ../src/heartbeat/note.js
     ../src/heartbeat/parse_events.js
     ../src/heartbeat/parse_time_events.js
     ../src/heartbeat/part.js
     ../src/heartbeat/playhead.js
     ../src/heartbeat/position.js
+    ../src/heartbeat/quantize_fixed-length.js
     ../src/heartbeat/sample.js
     ../src/heartbeat/samplepack.js
     ../src/heartbeat/scheduler.js
@@ -45,8 +41,8 @@ files="../src/license.js
     ../src/heartbeat/song_grid.js
     ../src/heartbeat/song_update.js
     ../src/heartbeat/track.js
+    ../src/heartbeat/transpose.js
     ../src/heartbeat/util.js
-    ../src/heartbeat/quantize_fixed-length.js
     ../src/heartbeat/close_module.js"
 
 
